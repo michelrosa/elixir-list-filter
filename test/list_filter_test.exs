269ddde03138ae98a930d4937ecb2a0913a43dc7
Michel Rosa <michel@codexa.com.br>
2021-08-30 14:55:25 -0300

    first test

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,17 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "odd count list elements" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      assert ListFilter.call(list) == 3
+    end
+
+    test "passing an empty list" do
+      list = []
+
+      assert ListFilter.call(list) == 0
+    end
   end
 end
